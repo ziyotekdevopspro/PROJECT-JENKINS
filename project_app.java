017af4b03afabc389c8b3f8ab665a711c4b82394
iamkhaihoang <khai.hoang@outlook.com>
2022-04-27 18:07:29 -0400

    test trigger

--- project_app.java
+++ project_app.java
@@ -2,10 +2,10 @@ public class project_app
 {   
 public static void main(String[] args)    
 	{           
-		System.out.print("Enter your firstname: ");   
+		System.out.print("Enter Firstname: ");   
 		//reading a string from the user  
 		String firstname = System.console().readLine();  
-		System.out.print("Enter your lastname: ");   
+		System.out.print("Enter Lastname: ");   
 		//reading a string from the user  
 		String lastname = System.console().readLine();  
 		String strhello = "Welcome " +firstname + " " + lastname;
