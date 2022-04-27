bc4028c4dffb7136a06b05519c553a345e52dc51
iamkhaihoang <khai.hoang@outlook.com>
2022-04-27 19:38:39 -0400

    Commit for representation

--- project_app.java
+++ project_app.java
@@ -8,7 +8,7 @@ public static void main(String[] args)
 		System.out.print("Enter Lastname: ");   
 		//reading a string from the user  
 		String lastname = System.console().readLine();  
-		String strhello = "Welcome " +firstname + " " + lastname;
+		String strhello = "Welcome and Hello " +firstname + " " + lastname;
         strhello = strhello + " to DEVOPS16 team.";
 		System.out.println(strhello);   
 	}   
